59f383f72e5fcf24ec318a6b300052a63edee6f1
Patrick Demers <patrickdemers6@gmail.com>
2021-03-29 11:30:43 -0500

    double available memory

--- terraform/modules/function/main.tf
+++ terraform/modules/function/main.tf
@@ -46,7 +46,7 @@ resource "google_cloudfunctions_function" "foodScraper" {
   name    = var.function_name
   runtime = "nodejs12"
 
-  available_memory_mb   = 128
+  available_memory_mb   = 256
   source_archive_bucket = google_storage_bucket.bucket.name
   source_archive_object = google_storage_bucket_object.zip.name
   trigger_http          = true
